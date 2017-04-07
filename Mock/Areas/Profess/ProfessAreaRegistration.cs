77317fde84d13002bfb10054e1f08abc9a2a5839
luutrangminh <luutrangminh@gmail.com>
2017-04-07 22:09:40 +0700

    update-code

---
+++ Mock/Areas/Profess/ProfessAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Mock.Areas.Profess
+{
+    public class ProfessAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Profess";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Profess_default",
+                "Profess/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
