4de900824d25c56da5486a6fc1f97c4ae61d1aec
luutrangminh <luutrangminh@gmail.com>
2017-04-07 17:09:06 +0700

    Init

---
+++ Mock/Areas/Administrator/Controllers/ProfessorManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mock.Controllers
+{
+    public class ProfessorManagerController : Controller
+    {
+        // GET: ProfessorManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
