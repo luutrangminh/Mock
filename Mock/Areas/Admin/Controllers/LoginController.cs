f15a3a14a368973132c1c96f2931d1c5a0f126d8
luutrangminh <luutrangminh@gmail.com>
2017-04-13 21:50:54 +0700

    finish-admintrator

---
+++ Mock/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mock.Areas.Admin.Models;
+
+namespace Mock.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        OnlineTestEntities db = new OnlineTestEntities();
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection f)
+        {
+            var username = f["Username"];
+            var password = f["Password"];
+            foreach (var item in db.Administrators)
+            {
+                if (username == item.Username)
+                {
+                    if(password == item.Password)
+                    {
+                        return RedirectToAction("Index","Professors");
+                    }
+                    else
+                    {
+                        ViewBag.Password = "Mật khẩu không chính xác. Vui lòng kiểm tra lại.";
+                    }
+                }
+                else
+                {
+                    ViewBag.Username = "Tài khoản không chính xác. Vui lòng kiểm tra lại.";
+                }
+
+            }
+            return View();
+        }
+    }
+}
