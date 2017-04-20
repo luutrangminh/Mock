6593e843a9f84b092095d8366da512d6a34d0744
nguyenthienthanh <nguyenthienthanh.kiyoshi@gmail.com>
2017-04-20 15:36:42 +0700

    @Added question & answer DAO

---
+++ DataAccess/Answer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class Answer
+    {
+        Connections con = new Connections();
+
+        public IDataReader Get(int id)
+        {
+            string queryStr = "SELECT Id, Answer, QuestionId, Status" +
+            "FROM Answer" +
+            "WHERE Id = " + id;
+            return con.ExecuteReader(queryStr);
+        }
+
+        public IDataReader Get(int questionId)
+        {
+            string queryStr = "SELECT Id, Answer, QuestionId, Status" +
+            "FROM Answer" +
+            "WHERE QuestionId = " + questionId;
+            return con.ExecuteReader(queryStr);
+        }
+
+        public IDataReader Add(int questionId, string answer, bool status)
+        {
+            string queryStr = "INSERT INTO [dbo].[Answer] " +
+                "([Answer], [QuestionId], [Status])" +
+                "VALUES (N'" + answer + "', " + questionId + ", " + status + ")";
+            return (IDataReader)con.ExecuteScalar(queryStr);
+        }
+
+        public void Update(string answer)
+        {
+            string queryStr = "UPDATE [dbo].[Answer] SET [Answer] = N'" + answer + "'";
+            con.ExecuteNonQuery(queryStr);
+        }
+
+        public void Update(int questionId)
+        {
+            string queryStr = "UPDATE [dbo].[Answer] SET [QuestionId] = " + questionId;
+            con.ExecuteNonQuery(queryStr);
+        }
+
+        public void Update(int status)
+        {
+            string queryStr = "UPDATE [dbo].[Answer] SET [Status] = " + status;
+            con.ExecuteNonQuery(queryStr);
+        }
+
+        public void Update(string answer, int questionId, bool status)
+        {
+            string queryStr = "UPDATE [dbo].[Answer] SET [Answer] = N'" + answer + "'" +
+                ", [QuestionId] = " + questionId +
+                ", [Status] = " + status;
+            con.ExecuteNonQuery(queryStr);
+        }
+
+        public void Delete(int id)
+        {
+            string queryStr = "DELETE FROM [dbo].[Answer] WHERE [Id] = " + id;
+            con.ExecuteNonQuery(queryStr);
+        }
+
+        public void Delete(string questionId)
+        {
+            string queryStr = "DELETE FROM [dbo].[Answer] WHERE [QuestionId] = '" + questionId + "'";
+            con.ExecuteNonQuery(queryStr);
+        }
+    }
+}
---
+++ DataAccess/Question.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class Question
+    {
+        Connections con = new Connections();
+
+        public IDataReader Get(int id)
+        {
+            string queryStr = "SELECT Id, Question, ProjectId" +
+            "FROM Question" +
+            "WHERE Id = " + id;
+            return con.ExecuteReader(queryStr);
+        }
+
+        public IDataReader Get(int projectId)
+        {
+            string queryStr = "SELECT Id, Question, ProjectId" +
+            "FROM Question" +
+            "WHERE ProjectId = " + projectId;
+            return con.ExecuteReader(queryStr);
+        }
+
+        public IDataReader Add(int projectId, string question)
+        {
+            string queryStr = "INSERT INTO [dbo].[Question] " +
+                "([Question], [ProjectId])" +
+                "VALUES (N'" + question + "', " + projectId + ")";
+            return (IDataReader)con.ExecuteScalar(queryStr);
+        }
+
+        public void Update(string question)
+        {
+            string queryStr = "UPDATE [dbo].[Question] SET [Question] = N'" + question + "'";
+            con.ExecuteNonQuery(queryStr);
+        }
+
+        public void Update(int projectId)
+        {
+            string queryStr = "UPDATE [dbo].[Question] SET [ProjectId] = " + projectId;
+            con.ExecuteNonQuery(queryStr);
+        }
+
+        public void Update(string question, int projectId)
+        {
+            string queryStr = "UPDATE [dbo].[Question] SET [Question] = N'" + question + "'" +
+                ", [ProjectId] = " + projectId;
+            con.ExecuteNonQuery(queryStr);
+        }
+
+        public void Delete(int id)
+        {
+            string queryStr = "DELETE FROM [dbo].[Question] WHERE [Id] = " + id;
+            con.ExecuteNonQuery(queryStr);
+        }
+
+        public void Delete(string projectId)
+        {
+            string queryStr = "DELETE FROM [dbo].[Question] WHERE [ProjectId] = '" + projectId + "'";
+            con.ExecuteNonQuery(queryStr);
+        }
+    }
+}
