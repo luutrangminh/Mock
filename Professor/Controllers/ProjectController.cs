d601a690a57df80d4f2b0b2f1ad226d4d7eed948
nguyenthienthanh <nguyenthienthanh.kiyoshi@gmail.com>
2017-04-22 22:57:21 +0700

    @Added demo professor

---
+++ Professor/Controllers/ProjectController.cs
@@ -0,0 +1,110 @@
+﻿using Professor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cryptography;
+
+namespace Professor.Controllers
+{
+    public class ProjectController : Controller
+    {
+        //
+        // GET: /Project/
+        //[Route("")]
+        public ActionResult Index()
+        {
+            var projectView = new ProjectViewModel();
+            projectView.name = "Test View";
+            projectView.code = _SHA1.Hash("ád");
+            return View(projectView);
+        }
+
+        //
+        // GET: /Project/Details/5
+        [Route("details")]
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Project/Create
+        [Route("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Project/Create
+        [HttpPost]
+        [Route("create")]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Project/Edit/5
+        [Route("edit/{id}")]
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Project/Edit/5
+        [HttpPost]
+        [Route("edit/{id}")]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Project/Delete/5
+        [Route("del/{id}")]
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Project/Delete/5
+        [HttpPost]
+        [Route("del/{id}")]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Professor/Models/ProjectViewModels.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Professor.Models
+{
+    public class ProjectViewModel
+    {
+        [Required]
+        [Display(Name = "Project name")]
+        public string name { get; set; }
+        [Display(Name = "Project code")]
+        public string code { get; set; }
+        [Display(Name = "Created At")]
+        public DateTime createdAt { get; set; }
+
+        [Display(Name = "Created By")]
+        public string createdBy { get; set; }
+        [Display(Name = "Start At")]
+        public DateTime startAt { get; set; }
+        [Display(Name = "Time")]
+        public int time { get; set; }
+    }
+
+    public class QuestionViewModel
+    {
+        [Required]
+        [Display(Name = "Question")]
+        public string question { get; set; }
+    }
+
+    public class AnswerViewModel
+    {
+        [Required]
+        [Display(Name = "Answer")]
+        public string answer { get; set; }
+        public bool status { get; set; }
+    }
+}
