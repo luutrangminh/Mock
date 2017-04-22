344ce04931e6dc30fe5c211e0bbc47b9563dbb20
nguyenthienthanh <nguyenthienthanh.kiyoshi@gmail.com>
2017-04-22 13:10:08 +0700

    @Added MapMvcAttributeRoutes

--- StudenManagement/Controllers/HomeController.cs
+++ StudenManagement/Controllers/HomeController.cs
@@ -8,11 +8,13 @@ namespace StudenManagement.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("")]
         public ActionResult Index()
         {
             return View();
         }
 
+        [Route("about")]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +22,7 @@ namespace StudenManagement.Controllers
             return View();
         }
 
+        [Route("contact")]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
